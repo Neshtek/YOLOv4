fa20628ae229ca1e9c27f456c5d8e42ee1d8b5c5
Neshtek <neelakshtayal@gmail.com>
2022-10-10 20:12:56 +0530

    new scripts and processed dataset

--- edit-labels.py
+++
@@ -1,7 +0,0 @@
-for i in range(228):
-    with open(r"C:\\Users\\neela\\Documents\\pseudoLabelGeneratorForYOLO\\yolo-labels\\frame%d.txt" % i, 'r+') as fp:
-        lines = fp.readlines()
-        fp.seek(0)
-        fp.truncate()
-        for number, line in enumerate(lines):
-            fp.write(line[1:])
